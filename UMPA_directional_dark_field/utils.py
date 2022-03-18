379b7bfdb5e9134444899d371e0b660dbf6b881f
ronaanbread <ronan.smith@soton.ac.uk>
2022-03-18 09:26:07 +0000

    updated directionality definition

--- UMPA_directional_dark_field/utils.py
+++ UMPA_directional_dark_field/utils.py
@@ -119,7 +119,7 @@ def generate_rgb(gauss_properties, log_scale=False):
 
     magnitude = np.sqrt(gauss_properties[:,:,0]**2 + gauss_properties[:,:,1]**2)
 
-    directionality = 1- (gauss_properties[:,:,1] / gauss_properties[:,:,0])
+    directionality = 1- ((gauss_properties[:,:,1]**2) / (gauss_properties[:,:,0]**2))
     directionality = np.nan_to_num(directionality)
 
     hsv = np.empty((3, gauss_properties.shape[0], gauss_properties.shape[1]))
